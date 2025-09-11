fc1cfae9c6813bb2aafd414468e39f00211e89c2
Administrator 777 <evgeniy.tolk.front@gmail.com>
2025-09-11 14:26:52 +0300

    создали категории и товары

---
+++ lesson_1_2/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+{
+  const item = 'умный проектор philips';
+  const count = 15;
+  const category = 'видеотехника';
+  const price = 1200;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+};
+
+{  
+  const item = 'iphone 15 pro';
+  const count = 300;
+  const category = 'мобильные телефоны';
+  const price = 30000;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
