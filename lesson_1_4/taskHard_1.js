6f7e875817e449a65d8a4d7e0ea5b728d005d0d2
Administrator 777 <evgeniy.tolk.front@gmail.com>
2025-09-11 15:52:57 +0300

    сделали 1_4

---
+++ lesson_1_4/taskHard_1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+{
+  const input = prompt('Введи свою зарплату');
+  const cash = Number(input); // явное преобразование к числу
+
+  if (Number.isNaN(cash) || cash <= 0) {
+    alert('Вы ввели некорректные данные');
+  } else if (cash < 15000) {
+    const nalog = 0.13 * cash;
+    console.log(`Ваш налог от зарплаты составляет ${nalog}`);
+  } else if (cash < 50000) {
+    const nalog = 0.2 * cash;
+    console.log(`Ваш налог от зарплаты составляет ${nalog}`);
+  } else {
+    const nalog = 0.3 * cash;
+    console.log(`Ваш налог от зарплаты составляет ${nalog}`);
+  }
+}
--- lesson_1_4/tasks.js
+++ lesson_1_4/tasks.js
@@ -5,6 +5,6 @@ const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 const rain = random(0,1);
-if(rain === 1) {console.log ('Пошел дождь. Возьмите зонт!')}
-else {console/log('Дождя нет')};
+if(rain === 1) {console.log (`Пошел дождь. Возьмите зонт!`)}
+else {console.log('Дождя нет')};
 };
