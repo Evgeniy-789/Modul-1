24243f7289dfa246370a9205deeef4dba7b357a0
Administrator 777 <evgeniy.tolk.front@gmail.com>
2025-09-17 22:11:58 +0300

    сделали 1.7

---
+++ lesson_1_7/task01.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const filter = (Array) => {
+    const bull = 
+}
+
+const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
+const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
